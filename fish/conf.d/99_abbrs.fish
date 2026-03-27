80c063a592ecc6563975bf1361bb2bf15a8711be
Rtosshy <tosshy820@gmail.com>
2026-03-27 12:08:16 +0900

    feat: telescope.nvim

--- fish/conf.d/99_abbrs.fish
+++ fish/conf.d/99_abbrs.fish
@@ -3,12 +3,10 @@ abbr -a sc "source $HOME/.config/fish/config.fish"
 
 # check nvim is installed or not
 if command -q nvim
-    echo "nvim is found"
     abbr -a v   "nvim"
     abbr -a vi  "nvim"
     abbr -a vim "nvim"
 else
-    echo "nvim is not found"
     abbr -a v   "vim"
     abbr -a vi  "vim"
     abbr -a vim "vim"
@@ -18,13 +16,11 @@ builtin command -s eza
 echo "exit code: $status"
 # check eza is installed or not
 if command -q eza
-    echo "eza is found"
     abbr -a ls "eza --icons"
     abbr -a ll "eza --icons -lhg --time-style long-iso"
     abbr -a la "eza --icons -lhag --time-style long-iso"
     abbr -a lt "eza --icons --tree"
 else
-    echo "eza is not found"
     abbr -a ls "ls -Fh"
     abbr -a ll "ls -lFh"
     abbr -a la "ls -laFh"
@@ -33,7 +29,6 @@ end
 
 # check ripgrep is installed or not
 if command -q rg
-    echo "ripgrep is found"
     abbr -a grep "rg"
 else
     echo "ripgrep is not found"
@@ -41,7 +36,6 @@ end
 
 # check bat is installed or not
 if command -q bat
-    echo "bat is found"
     abbr -a cat "bat"
 else
     echo "bat is not found"
