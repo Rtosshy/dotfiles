12d2c597136f7192469cc25edd3e4cdb33403558
Rtosshy <tosshy820@gmail.com>
2026-04-30 10:08:45 +0700

    chore(lazygit): add settings

--- nix-darwin/apps/lazygit.nix
+++ nix-darwin/apps/lazygit.nix
@@ -2,6 +2,16 @@
   home.file."Library/Application Support/lazygit/config.yml" = {
     force = true;
     text = ''
+      gui:
+        showIcons: true
+        nerdFontsVersion: "3"
+        border: single
+
+      git:
+        disableForcePushing: true
+
+      confirmOnQuit: true
+
       os:
         editPreset: nvim
     '';
