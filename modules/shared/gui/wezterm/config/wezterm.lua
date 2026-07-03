6aca3ca5057cdfc59046cc5190121832fb438f5b
Rtosshy <tosshy820@gmail.com>
2026-07-03 13:35:35 +0900

    fix: don't adust window size when changing font size

--- modules/shared/gui/wezterm/config/wezterm.lua
+++ modules/shared/gui/wezterm/config/wezterm.lua
@@ -8,6 +8,7 @@ config.keys = keybinds.keys
 config.key_tables = keybinds.key_tables
 
 config.automatically_reload_config = true
+config.adjust_window_size_when_changing_font_size = false
 config.font_size = 14.0
 config.use_ime = true
 config.window_background_opacity = 0.65
