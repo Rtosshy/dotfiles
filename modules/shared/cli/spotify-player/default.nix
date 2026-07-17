787f1e927ce2241a72f6e0dcdb3239824ad04528
Rtosshy <tosshy820@gmail.com>
2026-07-18 02:53:55 +0900

    chore: add spotify-player config

---
+++ modules/shared/cli/spotify-player/default.nix
@@ -0,0 +1,6 @@
+_: {
+  programs.spotify-player.enable = true;
+
+  xdg.configFile."spotify-player/app.toml".source = ./app.toml;
+  xdg.configFile."spotify-player/theme.toml".source = ./theme.toml;
+}
